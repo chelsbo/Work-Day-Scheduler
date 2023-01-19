20db9d173b89143512a316783c80f955dcf88501
chelsbo <chelsea.reese@icloud.com>
2023-01-19 12:05:39 -0700

    Update

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,37 +1,49 @@
-let currentDay = (new Date(year, month)).getDay();
+ $("#7am .description").val(localStorage.getItem("7am"));
+ $("#8am .description").val(localStorage.getItem("8am"));
+ $("#9am .description").val(localStorage.getItem("9am"));
+ $("#10am .description").val(localStorage.getItem("10am"));
+ $("#11am .description").val(localStorage.getItem("11am"));
+ $("#12pm .description").val(localStorage.getItem("12pm"));
+ $("#1pm .description").val(localStorage.getItem("1pm"));
+ $("#2pm .description").val(localStorage.getItem("2pm"));
+ $("#3pm .description").val(localStorage.getItem("3pm"));
+ $("#4pm .description").val(localStorage.getItem("4pm"));
+ $("#5pm .description").val(localStorage.getItem("5pm"));
 
-daysInMonth(iMonth, iYear) 
-{ 
-    // return 31 = new Date(iYear, iMonth, 31).getDate();
-};
+$(document).ready(function() {
+    var timeUpdate = function() {
+        $("#currentDate").text(moment().format("dddd, MMMM Do h:mm:ss a"));
+        $("#currentTime").text(moment().format("h:mm:ss a"));
+    };
+    timeUpdate();
+    setInterval(timeUpdate, 1000);
+    var colorCoder = function() {
+        var presentTime = moment().hours();
 
-let date = 1;
-    for (let i = 0; i < 6; i++) {
-        // creates a table row
-        let row = document.createElement("tr");
-
-        //creating individual cells, filing them up with data.
-        for (let j = 0; j < 7; j++) {
-            if (i === 0 && j < firstDay) {
-                cell = document.createElement("td");
-                cellText = document.createTextNode("");
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-            }
-            else if (date > daysInMonth(month, year)) {
-                break;
-            }
-
-            else {
-                cell = document.createElement("td");
-                cellText = document.createTextNode(date);
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-                date++;
-            }
-
-
+        $(".time-block").each(function() {
+        var timeBlock = parseInt($(this).attr("id")[1]);
+  
+        if (timeBlock < presentTime) {
+          $(this).addClass("past");
+        } 
+        else if (timeBlock === presentTime) {
+          $(this).addClass("present");
+        } 
+        else {
+          $(this).addClass("future");
         }
-
-        tbl.appendChild(row); // appending each row into calendar body.
+        console.log(timeBlock);
+      });   
     }
+    colorCoder();
+
+\    $(".saveBtn").on("click", function() {
+        var taskEntryTime = $(this).parent().attr("id");
+        var taskEntryText = $(this).siblings(".description").val();
+        localStorage.setItem(taskEntryTime, taskEntryText);
+        console.log(taskEntryTime, taskEntryText);
+    });
+})
+
+
+
